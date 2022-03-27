de86707a863a951b221904196dea2bf1ee71b0a8
kho903 <gmldnr2222@naver.com>
2022-03-27 12:31:46 +0900

    feat: #3 SortService 정렬 구현체를 사용하는 서비스를 별도로 작성 의존성 주입 방법을 적용

---
+++ java-oop/src/test/java/com/jikim/javaoop/service/SortServiceTest.java
@@ -0,0 +1,25 @@
+package com.jikim.javaoop.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import com.jikim.javaoop.logic.JavaSort;
+
+class SortServiceTest {
+
+	private SortService sortService = new SortService(new JavaSort<String>());
+
+	@Test
+	void test() {
+	    // given
+
+	    // when
+		List<String> answer = sortService.doSort(List.of("3", "2", "1"));
+
+		// then
+		assertEquals(List.of("1", "2", "3"), answer);
+	}
+}
